03b43488e1b67b18b031a86800c57453510a947c
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-05-14 21:31:09 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Menu from './Components/MenuComponent';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
