fb732ecb7e3fac31059046b9fe3c7ada55336ecb
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-05-29 21:26:41 +0530

    Assignment 3 solutions

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, ModalBody, ModalHeader, Modal, Row, Col, Label } from 'reactstrap';
 import { Link } from "react-router-dom";
+import { LocalForm, Control, Errors } from 'react-redux-form';
 
+    const required = (val) => val && val.length;
 
-    function RenderComments( {comments} ) {
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+
+    const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }    
+
+    handleSubmit(values) {
+        this.toggleModal();
+        alert("Current state is " + JSON.stringify(values));
+    }
+
+    RenderComments( comments ) {
+
         
         if (comments == null) {
             return (<div></div>)
@@ -35,13 +65,12 @@ import { Link } from "react-router-dom";
         )
     }
 
-    function RenderDish({ dish }) {
+    RenderDish( dish ) {
 
-        console.log("REnder Dish " + dish.image);
+        console.log("Render Dish " + dish.image);
 
         if (dish != null) {
             return (
-                <div className='col-12 col-md-5 m-1'>
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
@@ -49,40 +78,106 @@ import { Link } from "react-router-dom";
                             <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
-                </div>
+               
             )
         } else {
             return (<div></div>)
         }
     }
 
-    const DishDetail = (props) =>  {
-
-        if (props.dish == null) {
-            return (<div></div>)
-        }
+    render(){
 
         return (
             <div className = "container">
                 <div className="row">
                     <Breadcrumb>
                         <BreadcrumbItem> <Link to="/menu">Menu</Link> </BreadcrumbItem>
-                        <BreadcrumbItem active >{props.dish.name}</BreadcrumbItem>
+                        <BreadcrumbItem active >{this.props.dish.name}</BreadcrumbItem>
                     </Breadcrumb>
 
                     <div className="col-12">
-                        <h3>{props.dish.name}</h3>
+                        <h3>{this.props.dish.name}</h3>
                         <hr />
                     </div>
                 </div>
 
                 <div className = "row">
-                    <RenderDish dish = {props.dish}/>
-                    <RenderComments comments = {props.dish.comments}/>
+                    <div className="col-12 col-md-5 m-1">
+                        {this.RenderDish(this.props.dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {this.RenderComments(this.props.comments)}
+                        <div>
+                            <Button outline onClick = {this.toggleModal} >
+                                <span className = "fa fa-pencil fa-lg" />Submit Comment
+                            </Button>
+                            <Modal isOpen = {this.state.isModalOpen} toggle = {this.toggleModal}>
+                                <ModalHeader toggle = {this.toggleModal} > Submit Comment </ModalHeader>
+                                <ModalBody>
+                                    <LocalForm onSubmit = {(values) => this.handleSubmit(values)}>
+                                        <Row className="form-group">
+                                            <Label htmlFor="rating" md={12}>Rating</Label>
+                                            <Col md={12}>
+                                                
+                                                <Control.select model=".rating" name="rating"
+                                                    className="form-control">
+                                                    <option>1</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </Control.select>
+                                        
+                                            </Col>
+                                        </Row>
+                                        <Row className="form-group">
+                                            <Label htmlFor="author" md={12}>Your Name</Label>
+                                            <Col md={12}>
+                                                <Control.text model=".author" id="author" name="author"
+                                                    placeholder="Your Name"
+                                                    className="form-control"
+                                                    validators={{
+                                                        required, minLength: minLength(3), maxLength: maxLength(15)
+                                                    }}
+                                                     />
+                                                <Errors
+                                                    className="text-danger"
+                                                    model=".author"
+                                                    show="touched"
+                                                    messages={{
+                                                        required: 'Required',
+                                                        minLength: 'Must be greater than 2 characters',
+                                                        maxLength: 'Must be 15 characters or less'
+                                                    }}
+                                                 />
+                                            </Col>
+                                        </Row>
+
+                                        <Row className="form-group">
+                                            <Label htmlFor="comment" md={12}>Comment</Label>
+                                            <Col md={12}>
+                                                <Control.textarea model=".comment" id="comment" name="comment"
+                                                    rows="6"
+                                                    className="form-control" />
+                                            </Col>
+                                        </Row>
+
+                                        <Row className="form-group">
+                                            <Col md={{size:10, offset: 0}}>
+                                                <Button type="submit" color="primary">
+                                                Submit
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </LocalForm>
+                                </ModalBody>
+                            </Modal>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
-
+}
 
 export default DishDetail;
--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -41,7 +41,7 @@ class Header extends Component {
                     <div className = "container">
                         <NavbarToggler onClick = {this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src = "assets/images/logo.png" height = "30" width = "41" alt = "Restaurant Confusion" />
+                            <img src = "assets\images\logo.png" height = "30" width = "41" alt = "Restaurant Confusion" />
                         </NavbarBrand>
 
                         <Collapse isOpen = {this.state.isNavOpen} navbar>
